7ca29b4099507c55235aeb52416048bcc23f514e
Artyom Nikityuk <a.k.nikityuk@gmail.com>
2026-05-23 19:47:57 +0300

    Add classes for main logic of game to folder Core

---
+++ PingPongModalWindow/Model/Core/GameState(part 1).cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Core
+{
+    internal class GameState_part_1_
+    {
+    }
+}
