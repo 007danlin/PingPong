17934a47acfb7b0351cb4113c48fa1edfafe1665
Artyom Nikityuk <a.k.nikityuk@gmail.com>
2026-05-28 00:52:12 +0300

    Delete dot of names and write all logic GameState and Multiplayer

--- PingPongModalWindow/Model/Core/GameState.cs
+++ PingPongModalWindow/Model/Core/GameState.cs
@@ -1,12 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Model.Core.Abstract;
 
 namespace Model.Core
 {
-    internal class GameState_part_1_
+    public partial class GameState
     {
+        public BallBase Ball { get; set; }
+        public RacketBase Player1Racket { get; set; }
+        public RacketBase Player2Racket { get; set; }
+        public int ScorePlayer1 { get; set; }
+        public int ScorePlayer2 { get; set; }
+        public int MaxScore { get; set; }
+        public string SaveFolderPath { get; set; }
+        public bool IsMultiplayer { get; set; }
+
+        public GameState()
+        {
+            ScorePlayer1 = 0;
+            ScorePlayer2 = 0;
+            MaxScore = 11;
+        }
+
+        // Перегрузка оператора
+        public static GameState operator +(GameState a, GameState b)
+        {
+            return new GameState
+            {
+                ScorePlayer1 = a.ScorePlayer1 + b.ScorePlayer1,
+                ScorePlayer2 = a.ScorePlayer2 + b.ScorePlayer2,
+                MaxScore = a.MaxScore
+            };
+        }
+
+        // Перегрузка оператора
+        public static bool operator ==(GameState a, GameState b)
+        {
+            if (a is null && b is null) return true;
+            if (a is null || b is null) return false;
+            return a.ScorePlayer1 == b.ScorePlayer1 &&
+                   a.ScorePlayer2 == b.ScorePlayer2;
+        }
+
+        // Перегрузка оператора
+        public static bool operator !=(GameState a, GameState b)
+        {
+            return !(a == b);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is GameState other)
+                return this == other;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return ScorePlayer1.GetHashCode() ^ ScorePlayer2.GetHashCode();
+        }
     }
 }
---
+++ PingPongModalWindow/Model/Core/GameStateMultiplayer.cs
@@ -0,0 +1,42 @@
+﻿namespace Model.Core
+{
+    public partial class GameState
+    {
+        public string Player1Name { get; set; }
+        public string Player2Name { get; set; }
+        public bool Player1Ready { get; set; }
+        public bool Player2Ready { get; set; }
+
+        public void SetMultiplayer(string player1Name, string player2Name)
+        {
+            IsMultiplayer = true;
+            Player1Name = player1Name;
+            Player2Name = player2Name;
+            Player1Ready = false;
+            Player2Ready = false;
+        }
+
+        public bool BothPlayersReady()
+        {
+            return Player1Ready && Player2Ready;
+        }
+
+        public void SetPlayerReady(int playerNumber)
+        {
+            if (playerNumber == 1)
+                Player1Ready = true;
+            else if (playerNumber == 2)
+                Player2Ready = true;
+        }
+
+        public string GetWinner()
+        {
+            if (ScorePlayer1 > ScorePlayer2)
+                return Player1Name ?? "Игрок 1";
+            else if (ScorePlayer2 > ScorePlayer1)
+                return Player2Name ?? "Игрок 2";
+            else
+                return "Ничья";
+        }
+    }
+}
