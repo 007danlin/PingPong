c55058a324578ca3a199ded705f5dfd12795f142
Artyom Nikityuk <a.k.nikityuk@gmail.com>
2026-05-27 19:51:28 +0300

    Add GameEngine (main logic)

--- PingPongModalWindow/Model/Core/GameEngine.cs
+++ PingPongModalWindow/Model/Core/GameEngine.cs
@@ -12,13 +12,16 @@ namespace Model.Core
         private IRacket _player1Racket;
         private IRacket _player2Racket;
         private Timer _timer;
+        public bool IsMultiplayer { get; set; }
 
         public int ScorePlayer1 { get; private set; }
         public int ScorePlayer2 { get; private set; }
         public bool IsRunning { get; private set; }
         public bool BallInPlay { get; private set; }
 
+        // Делегат отвечает за забитое очко
         public Action<int, int> OnScoreChanged { get; set; }
+        // Делегат отвечает за завершение игры при победе одного из игроков
         public Action<string> OnGameOver { get; set; }
 
         public GameEngine(IGameField field, BallBase ball, IRacket player1, IRacket player2)
@@ -27,9 +30,10 @@ namespace Model.Core
             _ball = ball;
             _player1Racket = player1;
             _player2Racket = player2;
-
+            // типо счетчика fps
             _timer = new Timer(16);
             _timer.Elapsed += Update;
+            // перезапуск
             _timer.AutoReset = true;
         }
 
@@ -65,7 +69,11 @@ namespace Model.Core
             CheckWallCollision();
             CheckRacketCollision();
             CheckOutOfBounds();
+            if (!IsMultiplayer && BallInPlay)
+                MoveBot();
         }
+
+
 
         private void MoveBall()
         {
@@ -74,28 +82,55 @@ namespace Model.Core
             _ball.Y += _ball.Speed * Math.Sin(rad);
         }
 
+        private void MoveBot()
+        {
+            double botCenter = _player2Racket.Y + _player2Racket.Height / 2.0;
+            double ballCenter = _ball.Y + _ball.Radius;
+            double botSpeed = 3.5;
+
+            if (ballCenter < botCenter - 5)
+                _player2Racket.Move(-botSpeed);
+            else if (ballCenter > botCenter + 5)
+                _player2Racket.Move(botSpeed);
+
+            if (_player2Racket.Y < 0)
+                _player2Racket.Y = 0;
+            if (_player2Racket.Y + _player2Racket.Height > _field.Height)
+                _player2Racket.Y = _field.Height - _player2Racket.Height;
+        }
+
         private void CheckWallCollision()
         {
-            if (_ball.Y <= 0 || _ball.Y >= _field.Height - _ball.Radius * 2)
+            if (_ball.Y <= 0)
+            {
+                _ball.Y = 0;
                 _ball.Bounce(false);
+            }
+            if (_ball.Y >= _field.Height - _ball.Radius * 2)
+            {
+                _ball.Y = _field.Height - _ball.Radius * 2;
+                _ball.Bounce(false);
+            }
         }
 
         private void CheckRacketCollision()
         {
             if (_ball.X <= _player1Racket.X + _player1Racket.Width &&
                 _ball.X >= _player1Racket.X &&
-                _ball.Y >= _player1Racket.Y &&
+                _ball.Y + _ball.Radius * 2 >= _player1Racket.Y &&
                 _ball.Y <= _player1Racket.Y + _player1Racket.Height)
             {
+                _ball.X = _player1Racket.X + _player1Racket.Width;
                 _ball.Bounce(true);
                 _ball.Speed += _player1Racket.Power * 0.05;
             }
 
             if (_ball.X + _ball.Radius * 2 >= _player2Racket.X &&
-                _ball.X + _ball.Radius * 2 <= _player2Racket.X + _player2Racket.Width &&
-                _ball.Y >= _player2Racket.Y &&
+                _ball.X <= _player2Racket.X + _player2Racket.Width &&
+                _ball.Y + _ball.Radius * 2 >= _player2Racket.Y &&
                 _ball.Y <= _player2Racket.Y + _player2Racket.Height)
             {
+                _ball.X = _player2Racket.X - _ball.Radius * 2;
                 _ball.Bounce(true);
                 _ball.Speed += _player2Racket.Power * 0.05;
             }
@@ -107,6 +142,8 @@ namespace Model.Core
             {
                 ScorePlayer2++;
                 BallInPlay = false;
+                _ball.X = _field.Width / 2.0;
+                _ball.Y = _field.Height / 2.0;
                 OnScoreChanged?.Invoke(ScorePlayer1, ScorePlayer2);
                 CheckVictory();
             }
@@ -114,6 +151,8 @@ namespace Model.Core
             {
                 ScorePlayer1++;
                 BallInPlay = false;
+                _ball.X = _field.Width / 2.0;
+                _ball.Y = _field.Height / 2.0;
                 OnScoreChanged?.Invoke(ScorePlayer1, ScorePlayer2);
                 CheckVictory();
             }
